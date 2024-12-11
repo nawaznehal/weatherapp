fe0fed7db88c8fd90eaba9083688f58802a33fb4
Nawaz <nawaznehal786@gmail.com>
2024-12-11 12:47:19 -0500

    modify page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,6 @@ export default function Home() {
       const { data } = await axios.get(
         `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_API_KEY}&cnt=56`
       );
-      
       return data;
     }
   );
